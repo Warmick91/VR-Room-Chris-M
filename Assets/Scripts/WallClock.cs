f8f6b1993b47f6217f87fd6e92fed2985468932c
Krzysztof Mickiewicz <107584697+Warmick91@users.noreply.github.com>
2023-04-06 13:15:22 +0200

    The room and surroundings have been set up. The clock on the wall works using a custom script

---
+++ Assets/Scripts/WallClock.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class WallClock : MonoBehaviour
+{
+    [SerializeField] GameObject hourHand;
+    [SerializeField] GameObject minuteHand;
+    [SerializeField] GameObject secondHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateClock", 0.1f, 1f);
+    }
+
+    void UpdateClock()
+    {
+        DateTime time = DateTime.Now;
+
+        float hourHandAngle = (360f / 12f) * time.Hour;
+        float minuteHandAngle = (360f / 60) * time.Minute;
+        float secondHandAngle = (360 / 60) * time.Second;
+
+        hourHand.transform.localRotation = Quaternion.Euler(hourHandAngle, 0, 0);
+        minuteHand.transform.localRotation = Quaternion.Euler(minuteHandAngle, 0, 0);
+        secondHand.transform.localRotation = Quaternion.Euler(secondHandAngle, 0, 0);
+    }
+}
